cba4d2b083c5fa9fe103df17e090c093c6ad4a26
Wouter de Vos <wouter@codaisseur.com>
2018-03-02 10:29:25 +0100

    Simple API

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const Sequelize = require('sequelize')
+
+const app = express()
+
+var sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
+
+const Product = sequelize.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  image: Sequelize.STRING
+}, {
+  tableName: 'products',
+  timestamps: false
+})
+
+Product.findById(1).then(product => console.log(JSON.stringify(product)))
+
+app.listen(4001, () => {
+  console.log('Express API listening on port 4001')
+})
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+  next()
+})
+
+app.get('/products', (req, res) => {
+  Product.findAll({
+    attributes: ['id', 'name', 'price']
+  })
+    .then(result => {
+      res.json(result)
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({message: 'Something went wrong'})
+    })
+})
+
+app.get('/products/:id', (req, res) => {
+  Product.findById(req.params.id)
+    .then(result => {
+      if (result) {
+        res.json(result)
+      } else {
+        res.status(404)
+        res.json({ message: 'Not Found' })
+      }
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({ message: 'There was an error' })
+    })
+})
